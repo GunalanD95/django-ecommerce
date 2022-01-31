6c6a94d2748e32ce658d76693a368cfbee3a42a4
GunalanD95 <guna19may2015@gmail.com>
2022-01-31 08:21:04 +0530

    adding recent changes

--- esite/models.py
+++ esite/models.py
@@ -1,3 +1,29 @@
+from turtle import title
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class OrderCart(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.title
+
+
+class OrderItem(models.Model):
+    item_name = models.ForeignKey(OrderCart, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    ordered = models.BooleanField(default=False)
+    order_date = models.DateField(auto_now_add=True)
+    order_item = models.ManyToManyField(OrderItem)
+
+    def __str__(self):
+        return self.user.username
--- esite/urls.py
+++ esite/urls.py
@@ -4,4 +4,5 @@ from . views import *
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('accounts/', include('allauth.urls')),
 ]
