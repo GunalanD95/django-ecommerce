e105f3d4321e424eb95fd3efa07e3b16cb607fb8
GunalanD95 <guna19may2015@gmail.com>
2022-02-06 15:32:23 +0530

    pushing recent changes

---
+++ esite/static/js/cart.js
@@ -0,0 +1,9 @@
+// console.log("jsjfosdjfdjififhsiusbd");
+
+// var add_cart = document.getElementById("add-cartj");
+
+// // on click of add_cart link console.log("add_cart");
+
+// add_cart.addEventListener("click", function(e) {
+//     console.log("add_cart");
+// });
