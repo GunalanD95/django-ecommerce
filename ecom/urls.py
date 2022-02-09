418ffd9aebe73c569ef92ea42ce6bf9249c177a7
GunalanD95 <guna19may2015@gmail.com>
2022-02-09 06:47:59 +0530

    modify fix static

--- ecom/urls.py
+++ ecom/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls.static import static
+from django.urls import include, re_path
+from django.views.static import serve
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("esite.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
