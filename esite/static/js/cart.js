a7d15f5652b32701f032a0be3837e8b5ccb24b76
GunalanD95 <guna19may2015@gmail.com>
2022-02-14 07:30:43 +0530

    add recent changes

--- esite/static/js/cart.js
+++ esite/static/js/cart.js
@@ -1,11 +1,23 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//     var rowCount = document.getElementsByTagName("tbody")[0].rows;
-//     lengths = rowCount.length;
-//     var cartlen = lengths;
-
-//     var rcounter = document.getElementById("cart-counter");
-//     rcounter.innerHTML = cartlen;
-//     console.log(cartlen);
-// });
+document.addEventListener("DOMContentLoaded", function(){
+    // check if pay-btn is clicked and then load data to payment page
+    document.getElementById("pay-btn").addEventListener("click", function(){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:8080/esite/cart/getCartData", true);
+        xhr.onload = function(){
+            if(this.status == 200){
+                var data = JSON.parse(this.responseText);
+                var total = 0;
+                var html = "";
+                for(var i = 0; i < data.length; i++){
+                    total += data[i].price * data[i].quantity;
+                    html += "<tr><td>" + data[i].name + "</td><td>" + data[i].price + "</td><td>" + data[i].quantity + "</td><td>" + data[i].price * data[i].quantity + "</td></tr>";
+                }
+                document.getElementById("cart-table").innerHTML = html;
+                document.getElementById("total").innerHTML = total;
+            }
+        }
+        xhr.send();
+    });
+});
 
 
